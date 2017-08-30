731edd64a635b4b9a6d5d1e084ac33ff983a6303
blaize9 <blaize9@gmail.com>
2017-08-30 17:12:07 -0500

    Add Godep support

---
+++ vendor/github.com/j-muller/go-torrent-parser/types.go
@@ -0,0 +1,61 @@
+package gotorrentparser
+
+import (
+	"time"
+
+	"github.com/zeebo/bencode"
+)
+
+type FileMetadata struct {
+	Path   []string `bencode:"path"`
+	Length int64    `bencode:"length"`
+}
+
+type InfoMetadata struct {
+	PieceLength int64  `bencode:"piece length"`
+	Pieces      []byte `bencode:"pieces"`
+
+	// single file context
+	Name   string `bencode:"name"`
+	Length int64  `bencode:"length"`
+
+	// multi file context
+	Files bencode.RawMessage `bencode:"files"`
+}
+
+type Metadata struct {
+	// Foobar   []interface{} `bencode:"announce-list"`
+	Announce     string             `bencode:"announce"`
+	AnnounceList [][]string         `bencode:"announce-list"`
+	Comment      string             `bencode:"comment"`
+	CreatedBy    string             `bencode:"created by"`
+	CreatedAt    int64              `bencode:"creation date"`
+	Info         bencode.RawMessage `bencode:"info"`
+}
+
+type File struct {
+	// Relative path of the file
+	Path []string
+
+	// File length
+	Length int64
+}
+
+type Torrent struct {
+	// Announce URL
+	Announce []string
+
+	// Torrent comment
+	Comment string
+
+	// Author
+	CreatedBy string
+
+	// Creation time
+	CreatedAt time.Time
+
+	// Torrent SHA1
+	InfoHash string
+
+	Files []*File
+}
